001cecc945c112a995581a80f5fb6c0864a86ea8
Le-Shunny <carandangzane13@gmail.com>
2025-11-29 23:58:04 +0800

    U48B2M2

--- src/components/RulesModal.tsx
+++ src/components/RulesModal.tsx
@@ -49,13 +49,6 @@ export default function RulesModal({ isOpen, onClose, type }: RulesModalProps) {
             ))}
           </ol>
         </div>
-        <div className="flex justify-end p-6 border-t">
-          <button
-            onClick={onClose}
-            className="px-4 py-2 bg-blue-600 text-white rounded-md hover:bg-blue-700 transition"
-          >
-            Close
-          </button>
         </div>
       </div>
     </div>
